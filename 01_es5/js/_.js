3f47c93c9e87ee72c4b10d30b286707d5288b686
201411096 <korea5781@gmail.com>
2021-04-23 14:55:29 +0900

    210423_01

--- 01_es5/js/_.js
+++ 01_es5/js/_.js
@@ -42,3 +42,35 @@ function _curryr(fn){
 var _get = _curryr(function(obj, key){
     return obj == null ? undefined : obj[key];
 });
+
+var _map = _curryr(_map);
+var _filter = _curryr(_filter);
+
+function _rest(list, num){
+    return slice.call(list, num || 1);  // num이 없다면 기본값 1로..
+}
+
+function _reduce(list, iter, memo){
+    if(arguments.length == 2){
+        memo = list[0];
+        list = _rest(list); // 맨 앞의 요소를 하나 자름
+    }
+    _each(list, function(val){
+        memo = iter(memo, val);
+    });
+    return memo;
+}
+
+function _pipe(){
+    var fns = arguments;
+    return function(arg){
+        return _reduce(fns, function(arg, fn){
+            return fn(arg);
+        }, arg);
+    }
+}
+
+function _go(arg){                  // 실행시 함수를 포함해서 여러 인자를 넣게 되는데, 함수리스트는 arguments에서 맨 앞 인자를 빼서 추출하고, 첫 인자만 arg에 담겨져서 넘어가는 형태
+    var fns = _rest(arguments);
+    _pipe.apply(null, fns)(arg);
+}
